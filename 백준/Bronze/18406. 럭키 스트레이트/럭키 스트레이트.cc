85ecab6b1ec35c5a4290d728cc91b5649a9eb830
yunho09 <jyh19310302@gmail.com>
2025-06-16 17:22:13 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.cc
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.cc
@@ -7,10 +7,8 @@ int main() {
 	int a = 0, b = 0;
 	int leng = strlen(n) / 2;
 	for (int i = 0; i < leng; i++) {
-		a += (n[i]-48);
-	}
-	for (int i = leng; i < leng*2; i++) {
-		b += (n[i] - 48);
+		a += (n[i] - 48);
+		b += (n[i+leng] - 48);
 	}
 	if (a == b) printf("LUCKY");
 	else printf("READY");
