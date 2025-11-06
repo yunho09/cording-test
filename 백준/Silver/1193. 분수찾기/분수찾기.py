9934b78a71a83fe856f8946f6a956055e8927f2b
JANG YUN HO <jyh19310302@gmail.com>
2025-11-06 20:19:00 +0900

    [Silver V] Title: 분수찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,15 @@
+import sys
+x = int(sys.stdin.readline())
+line = 1
+a = 1
+b = 1
+while x > line:
+  x -= line
+  line += 1
+if line % 2 == 0:
+  a = x
+  b = line - x + 1
+else:
+  a = line - x + 1
+  b = x
+print(f'{a}/{b}')
