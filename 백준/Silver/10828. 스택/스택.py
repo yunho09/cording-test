37dc9619c33c20d417b0e507a52d2316b3a70281
JANG YUN HO <jyh19310302@gmail.com>
2025-11-12 17:12:50 +0900

    [Silver IV] Title: 스택, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+stack = deque()
+a = int(sys.stdin.readline())
+for _ in range(a):
+  b = sys.stdin.readline().split()
+  if b[0] == 'push':
+    if len(b) > 1:
+      stack.append(b[1])
+  elif b[0] == 'pop':
+    if len(stack) >= 1:
+      print(stack.pop())
+    else:
+      print(-1)
+  elif b[0] == 'size':
+    print(len(stack))
+  elif b[0] == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+  elif b[0] == 'top':
+    if len(stack) >= 1:
+      print(stack[len(stack) - 1])
+    else:
+      print(-1)
