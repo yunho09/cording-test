5e046dc44eb3b1793cdbcbb5de555f4f2e163e5c
JANG YUN HO <jyh19310302@gmail.com>
2025-12-23 14:01:16 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 396 ms, Memory: 127776 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,9 @@
+import sys
+n = int(sys.stdin.readline())
+numlist = []
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    numlist.append([a, b])
+numlist.sort(key=lambda x : (x[1], x[0]))
+for i in numlist:
+    print(i[0], i[1])
